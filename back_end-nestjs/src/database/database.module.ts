1d3292957e2d5d1a83fc35ade947374012c5a23a
Kaito0208 <minhtranplus.0208@gmail.com>
2021-07-23 21:30:19 +0700

    update backend nodejs

---
+++ back_end-nestjs/src/database/database.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
+
+@Module({
+    imports: [TypeOrmModule.forRoot()],
+    exports: [TypeOrmModule]
+})
+export class DatabaseModule {
+    constructor(connection: Connection) {
+        if(connection.isConnected)
+        console.log("DB Connected successfully");
+    }
+}
